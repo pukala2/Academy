8b1e4ec48699d49de91aa145d2710db151b264ef
pukala2 <41795180+pukala2@users.noreply.github.com>
2019-01-29 20:44:53 +0100

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+//g++  5.4.0
+
+#include <iostream>
+
+class Person
+{
+private:
+    int age;
+    char* name;
+public:
+    // napisz konstruktor
+    // napisz konstruktor kopiujący
+    // napisz metode wyswietlajaca dane
+};
+
+int main()
+{
+    Person x{22, "Juzek"};
+}
